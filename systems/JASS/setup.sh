ba2e4ddfbb5bd89ca8c0c7ff6a7480d664d07e8a
lintool <jimmylin@uwaterloo.ca>
2019-10-07 22:53:46 -0400

    JASS: added local tarball.

--- systems/JASS/setup.sh
+++ systems/JASS/setup.sh
@@ -8,8 +8,11 @@ if [[ ! -d ${ATIRE_DIR} ]]; then
 fi
 
 if [[ ! -d JASS ]]; then
-	git clone https://github.com/lintool/JASS.git
-	git checkout -q b27b319
+        # As of Oct 7, 2019, this remote repo still exists...
+	# git clone https://github.com/lintool/JASS.git
+	# git checkout -q b27b319
+	# ... but it's still safer to have a local copy...
+	tar xvfz JASS.tar.gz
 fi
 
 cd JASS
