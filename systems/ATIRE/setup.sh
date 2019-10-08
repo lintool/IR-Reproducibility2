fbc1cbbfbeb8cace112f32378286a10a5c31a910
lintool <jimmylin@uwaterloo.ca>
2019-10-07 22:33:16 -0400

    ATIRE: added local tarball.

--- systems/ATIRE/setup.sh
+++ systems/ATIRE/setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 if [[ ! -d "atire" ]]; then
-	hg clone http://www.atire.org/hg/atire -r f3102a7a5848
+    # As of Oct 7, 2019, this remote repo still exists...
+    # hg clone http://www.atire.org/hg/atire -r f3102a7a5848
+    # ... but it's still safer to have a local copy...
+    tar xvfz atire.tar.gz
 fi
 
 cd atire
